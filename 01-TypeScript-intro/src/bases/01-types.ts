b8033d08a29830715f10ec3e2372db84ad582baa
Jgomcar <joangomez2001.gc@gmail.com>
2023-08-14 18:20:51 +0200

    TypeScript Introducction created

---
+++ 01-TypeScript-intro/src/bases/01-types.ts
@@ -0,0 +1,9 @@
+// si no se exporta algo, da error.
+export const name = 'Joan';
+export const age: number = 35;
+export const isValid: boolean = true;
+
+//si ponemos código ejecutable, cuando se importa desde otro archivo se ejecuta el código
+console.log(name, age)
+
+
