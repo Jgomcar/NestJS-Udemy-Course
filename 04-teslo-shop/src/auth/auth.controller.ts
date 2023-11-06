24922694d24087bb5e88de2b5df712cf23973bf1
Jgomcar <joangomez2001.gc@gmail.com>
2023-11-06 23:46:58 +0100

    185

--- 04-teslo-shop/src/auth/auth.controller.ts
+++ 04-teslo-shop/src/auth/auth.controller.ts
@@ -1,7 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, Req, SetMetadata } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
+import { GetUser, RawHeaders } from './decorators';
 import { CreateUserDto, LoginUserDto } from './dto';
+import { User } from './entities/user.entity';
+import { UserRoleGuard } from './guards/user-role.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -20,9 +23,31 @@ export class AuthController {
   @Get('private')
   @UseGuards( AuthGuard() )
   testingPrivateRoute(
-    @Req() request: Express.Request
+    @Req() request: Express.Request,
+    @GetUser() user: User,
+    @GetUser('email') userEmail: string,
+
+    @RawHeaders() rawHeaders: string[],
   ) {
     console.log(request);
+    console.log(user)
+    return {
+      user,
+      userEmail,
+      rawHeaders
+    }
+  }
+
+  @Get('private2')
+  @SetMetadata('roles', ['admin', 'super-user'])
+  @UseGuards( AuthGuard() , UserRoleGuard )
+  privateRoute2(
+    @GetUser() user: User
+  ) {
+    return {
+      ok: true,
+      user
+    }
   }
 
 }
---
+++ 04-teslo-shop/src/auth/decorators/index.ts
@@ -0,0 +1,4 @@
+export { RawHeaders } from "./get-rawHeaders.decorator";
+export { GetUser } from "./get-user.decorator";
+export { RoleProtected } from "./role-protected.decorator";
+
