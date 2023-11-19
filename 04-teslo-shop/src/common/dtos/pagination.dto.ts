8666b6f2961e966ef57b5dc9e67867644cbdb281
Jgomcar <joangomez2001.gc@gmail.com>
2023-11-19 19:35:06 +0100

    seccion de documentacion acabada

--- 04-teslo-shop/src/common/dtos/pagination.dto.ts
+++ 04-teslo-shop/src/common/dtos/pagination.dto.ts
@@ -1,12 +1,22 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Type } from "class-transformer";
 import { IsOptional, IsPositive, Min } from "class-validator";
 
 export class PaginationDto {
+
+    @ApiProperty({
+        default: 10,
+        description: 'How many rows do you want'
+    })
     @IsOptional()
     @IsPositive()
     @Type( () => Number ) // otra forma de convertir el queryparam en number
     limit?: number;
 
+    @ApiProperty({
+        default: 0,
+        description: 'How many rows do you want to skip'
+    })
     @IsOptional()
     @Type( () => Number )
     @Min(0)
--- 04-teslo-shop/src/products/entities/product.entity.ts
+++ 04-teslo-shop/src/products/entities/product.entity.ts
@@ -2,46 +2,85 @@
 import { User } from "../../auth/entities/user.entity";
 import { BeforeInsert, BeforeUpdate, Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { ProductImage } from './product-image.entity';
+import { ApiProperty } from "@nestjs/swagger";
 
 @Entity({ name: 'products' })
 export class Product {
+
+    @ApiProperty({ 
+        example: '229cd311-f521-4357-877f-85feec12bba5',
+        description: 'Product id',
+        uniqueItems: true
+     })
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
+    @ApiProperty({ 
+        example: 'T-Shirt Teslo',
+        description: 'Product Title',
+        uniqueItems: true
+     })
     @Column('text', {
         unique: true,
     })
     title: string;
 
+    @ApiProperty({ 
+        example: 0,
+        description: 'Product Price',
+     })
     @Column('float',{
         default: 0,
     })
     price: number;
 
+    @ApiProperty({ 
+        example: 'Description',
+        description: 'Product Description',
+     })
     @Column({
         type: 'text',
         nullable: true,
     })
     description: string;
-   
+
+    @ApiProperty({ 
+        example: 'T-Shirt Teslo',
+        description: 'Product SLUG - for SEO routes',
+        uniqueItems: true
+     })
     @Column('text', {
         unique: true,
     })
     slug: string;
 
+    @ApiProperty({ 
+        example: 10,
+        description: 'Product stock',
+        default: 0
+     })
     @Column('int',{
         default: 0,
     })
     stock: number;
 
+    @ApiProperty({ 
+        example: ['M', 'XL', 'XXL'],
+        description: 'Product size'
+     })
     @Column('text', {
         array: true,
     })
     sizes: string[]
 
+    @ApiProperty({ 
+        example: 'man',
+        description: 'Product gender'
+     })
     @Column('text')
     gender: string;
 
+    @ApiProperty()
     @Column('text', {
         array: true,
         default: []
@@ -50,6 +89,7 @@ export class Product {
     
     
     // images
+    @ApiProperty()
     @OneToMany(
         () => ProductImage,
         ( productImage ) => productImage.product,
